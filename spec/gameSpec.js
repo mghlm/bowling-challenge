f428331de487beaf06883772b640e5cce65e4345
Magnus Holm <mail@magnusholm.com>
2017-04-29 15:21:31 +0100

    add secondRoll function and totalFrameScore to frames

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -5,23 +5,30 @@ describe ('Game', function() {
     game = new Game();
   });
 
-  it('starts with a score of 0', function() {
-    expect(game.score).toEqual(0);
+  it('starts with a total score of 0', function() {
+    expect(game.totalScore).toEqual(0);
   });
 
-  it('increase the score to the first number', function() {
+  it('increase the total score to the first number', function() {
     game.firstRoll(7);
-    expect(game.score).toEqual(7);
+    expect(game.totalScore).toEqual(7);
     expect(game.firstFrameRoll1).toEqual(7);
   });
 
-  it('doesn\'t increase the score if the first roll is 10', function() {
+  it('adds an X to the first roll box and total score for the frame if the first roll is 10', function() {
     game.firstRoll(10);
-    expect(game.score).toEqual(10);
+    expect(game.totalScore).toEqual(10);
     expect(game.firstFrameRoll1).toEqual("X");
+    expect(game.totalFirstFrame).toEqual("X");
   });
 
-  
+  it('adds second rolls score to first rolls score if they combined are less than 10', function() {
+    game.firstRoll(3);
+    game.secondRoll(4);
+    expect(game.firstFrameRoll2).toEqual(4);
+    expect(game.totalFirstFrame).toEqual(7);
+  });
+
 
 
 
--- src/game.js
+++ src/game.js
@@ -1,22 +1,37 @@
 var Game = function() {
-  this.score = 0;
+  this.totalScore = 0;
 
   this.firstFrameRoll1 = 0;
   this.firstFrameRoll2 = 0;
+  this.totalFirstFrame = 0;
 
   this.secondFrameRoll1 = 0;
   this.secondFrameRoll2 = 0;
+  this.totalSecondFrame = 0;
 
   this.thirdFrameRoll1 = 0;
   this.thirdFrameRoll2 = 0;
+  this.totalThirdFrame = 0;
 
   this.fourthFrameRoll1 = 0;
   this.fourthFrameRoll2 = 0;
+  this.totalFourthFrame = 0;
 
   this.fifthFrameRoll1 = 0;
   this.fifthFrameRoll2 = 0;
+  this.totalFifthFrame = 0;
 };
 
 Game.prototype.firstRoll = function(pins) {
-  (pins === 10) ? (this.score = 10) && (this.firstFrameRoll1 = "X") : (this.score += pins) && (this.firstFrameRoll1 += pins);
+  (pins === 10) ?
+  (this.totalScore = 10) && (this.firstFrameRoll1 = "X") && (this.totalFirstFrame = "X") :
+  (this.totalScore += pins) && (this.firstFrameRoll1 += pins);
+};
+
+Game.prototype.secondRoll = function(pins) {
+  this.firstFrameRoll2 = pins;
+  this.totalFirstFrame = (this.firstFrameRoll1 + this.firstFrameRoll2);
+
+
+
 };
